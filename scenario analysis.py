338685f0a528b4e47125125c004eb25f6e461a78
andrukth <andru@kth.se>
2021-09-21 17:57:42 +0200

    Added private funds to the model!
    Added private funds to the model, main change.
    I also started working on a "scenario analysis" python file to display the scenarios. I also made two scenarios + base scenario in the excel, that picture the private-public sector willingness to be on board

---
+++ scenario analysis.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 21 17:18:49 2021
+
+@author: andru
+"""
+
+import pandas as pd
+#import os
+
+
+base_scenario = pd.read_csv('C:/Users/andru/Downloads/base scenario.csv')
